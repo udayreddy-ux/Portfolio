ca017ad296f11e56d4dc2d0201b8cf87caa46e09
K Uday <kovvurideveswarreddy@gmail.com>
2024-08-28 19:25:06 -0400

    Frontend sucessfull

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,5 +1,40 @@
+import Footer from './Footer';
+import React from 'react';
 export default function Contact(){
     return(
-        <h1>This is Contact page!!</h1>
+        <>
+        <div className='contact-form'>
+            <h1 className='head'>Let's Talk</h1>
+            <form>
+                <div className='form-group'>
+                    <div className='form-field'>
+                        <label>Name</label>
+                        <input type='text' placeholder='Your name' required/>
+                    </div>
+                </div>
+                <div className='form-group'>
+                    <div className='form-field'>
+                        <label>Email</label>
+                        <input type='email' placeholder='Your email' required/>
+                    </div>
+                </div>
+                <div className='form-group'>
+                    <div className='form-field'>
+                        <label>Subject</label>
+                        <input type="text" placeholder='Your subject' required/>
+                    </div>
+                </div>
+                <div className='form-group'>
+                    <div className='form-field'>
+                        <label>Message</label>
+                        <textarea placeholder='Write your message' required/>
+                    </div>
+                </div>
+                <br/>
+                <button type="submit">Send Message</button>
+            </form>
+        </div>
+        <Footer/>
+        </>
     );
 }
